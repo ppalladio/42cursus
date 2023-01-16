28893f4a9a9e9784dc8d69add6db518b613af5f1
Yuxuan Peng <nine.slide@gmail.com>
2023-01-16 19:07:16 +0100

    strlen(question

--- libft/libft.h
+++ libft/libft.h
@@ -6,15 +6,15 @@
 /*   By: yupeng <yupeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:08:50 by yupeng            #+#    #+#             */
-/*   Updated: 2023/01/11 16:07:31 by yupeng           ###   ########.fr       */
+/*   Updated: 2023/01/16 18:50:29 by yupeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef LIBFT_H
 # define LIBFT_H
-# include<ctype.h>
-# include <stdlib.h>
-# include <strings.h>
+# include <ctype.h>
+# include <stdio.h>
+# include <string.h>
 
 int		ft_isprint(int c);
 int		ft_isalpha(int c);
@@ -25,11 +25,11 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dest, const void * src, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-size_t	strlen(const char *s);
-size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memmove(void *dst, const void *src, size_t len);
@@ -38,7 +38,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_isdigit(int c);
-size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 // with malloc
 char	*ft_strdup(const char *s1);
@@ -58,13 +58,13 @@ void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 
 //: bonus:
-t_list *ft_lstnew(void *content);
-void ft_lstadd_front(t_list **lst, t_list *new);
-int ft_lstsize(t_list *lst);
-t_list *ft_lstlast(t_list *lst);
-void ft_lstadd_back(t_list **lst, t_list *new);
-void ft_lstdelone(t_list *lst, void (*del)(void *));
-void ft_lstclear(t_list **lst, void (*del)(void *));
-void ft_lstiter(t_list *lst, void (*f)(void *));
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+// t_list *ft_lstnew(void *content);
+// void ft_lstadd_front(t_list **lst, t_list *new);
+// int ft_lstsize(t_list *lst);
+// t_list *ft_lstlast(t_list *lst);
+// void ft_lstadd_back(t_list **lst, t_list *new);
+// void ft_lstdelone(t_list *lst, void (*del)(void *));
+// void ft_lstclear(t_list **lst, void (*del)(void *));
+// void ft_lstiter(t_list *lst, void (*f)(void *));
+// t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif 
