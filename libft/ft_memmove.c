ba3eb4eee3d8a76acb16446736a315e2ee16e03a
ppalladio <nine.slide@gmail.com>
2023-01-27 20:32:08 +0100

    add list to libft.h main to memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,6 +1,26 @@
 
 #include "libft.h"
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*dst;
+	char	*src_;
 
+	i = 0;
+	dst = (char *)dest;
+	src_ = (char *)src;
+	if (dst == src_)
+		return (NULL);
+	else
+	{
+		while (dst[i] && i < n)
+		{
+			dst[i] = src_[i];
+			i++;
+		}
+		return (dst);
+	}
+}
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t		i;
@@ -20,4 +40,20 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		}
 	 }
 	return (d);
+}
+
+int main()
+{
+	char dest[] = "olding";
+   	const char src[]  = "newstring";
+	size_t i = 11;
+	
+	printf("Before ft_memmove dest = %s, src = %s\n", dest, src);
+
+	printf("%p\n",ft_memmove(dest,src,i));
+
+   printf("After memmove dest = %s, src = %s\n", dest, src);
+	printf("%p\n",memmove(dest,src,i));
+
+	return 0;
 }
