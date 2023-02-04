787508f6841d758c4c2abdaf915d49e63b5bfbeb
ppalladio <nine.slide@gmail.com>
2023-02-04 23:50:17 +0100

    substr WIP

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -9,12 +9,26 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int len_s;
 
 	len_s = 0;
-	while(s[len_s])
+	while (s[len_s])
 		len_s++;
-	i=0;
+	i = 0;
 	sub = (char *)s;
-	sub = (char *)malloc(sizeof(char)*(len_s-start+1));
-	if (sub== NULL)
+	sub = (char *)malloc(sizeof(char) * (len_s - start));
+	if (sub == NULL)
 		return NULL;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	return sub;
+}
 
+int main()
+{
+	const char *s = "abcde";
+	unsigned int i = 3;
+	size_t len = 3;
+
+	printf("%p\n",ft_substr(s,i,len));
 }
