0c6a915d6a3d466a2a0b33fc0dbc0872e89a8241
Yuxuan Peng <nine.slide@gmail.com>
2023-02-08 17:57:26 +0100

    putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,14 +1,24 @@
-// #include "libft.h"
+#include "libft.h"
 
-// void ft_putnbr_fd(int n, int fd)
-
-// {
-// 	fd = 1;
-// 		write(fd, itoa(n), 1);
-// }
-
-// int main()
-// {
-// 	ft_putnbr_fd(0,1);
-// 	return 0;
-// }
+void ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n <= 9)
+	{
+		ft_putchar_fd(n + '0', fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+}
