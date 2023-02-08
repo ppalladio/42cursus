1c9104b51e6e3d39c1428185f6acef994085c0ed
Yuxuan Peng <nine.slide@gmail.com>
2023-02-08 19:39:19 +0100

    split

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yupeng <yupeng@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 18:21:52 by yupeng            #+#    #+#             */
+/*   Updated: 2023/02/08 18:38:31 by yupeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int res;
+	int	i;
+	int	sign;
+	int	res;
 
 	sign = 1;
 	i = 0;
@@ -16,13 +27,11 @@ int ft_atoi(const char *str)
 		sign = -1;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-
 	return (res * sign);
 }
 
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -34,4 +34,10 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 // }
 
 
-// This implementation starts by checking if the input string s is NULL. If it is, the function returns NULL. Then it checks if the start index is greater than or equal to the length of the input string. If it is, the function returns an empty string "". If both of these checks pass, the function uses malloc to allocate memory for the new substring. If the allocation fails, the function returns NULL. Finally, the function copies len characters from the input string starting at index start into the newly allocated memory and adds a null terminator '\0' to the end of the substring.
+// This implementation starts by checking if the input string s is NULL. If it is, the function returns NULL. 
+// Then it checks if the start index is greater than or equal to the length of the input string. 
+// If it is, the function returns an empty string "". 
+// If both of these checks pass, the function uses malloc to allocate memory for the new substring. 
+// If the allocation fails, the function returns NULL. 
+// Finally, the function copies len characters from the input string starting at index start 
+// into the newly allocated memory and adds a null terminator '\0' to the end of the substring.
