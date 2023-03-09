abf1949d462ce8c6872edf08ef8077220e9a625b
Yuxuan <nine.slide@gmail.com>
2023-03-09 14:28:00 +0100

    strncmp have to use unsigned char because working binary data, in Notion, notes

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -11,26 +11,28 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "libft.h"
+
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
 
-    i = 0;
-	if (!n)
-        return 0;
-    while (i < n && s1[i] && s2[i])
-    {
-        if (s1[i] < s2[i])
-            return -1;
-        else if (s1[i] > s2[i])
-            return 1;
-        i++;
-    }
-    if (i == n)
-        return 0;
-    return ((unsigned char )s1[i] - (unsigned char )s2[i]);
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
+		else if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (1);
+		else if ((unsigned char)s1[i] == '\0')
+			return (0);
+		i++;
+	}
+	return 0;
 }
+
 
 // int main()
 // {
