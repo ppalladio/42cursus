a75f6c95b00acffd72e500bc02d70591908b8823
ppalladio <nine.slide@gmail.com>
2023-04-04 19:17:14 +0200

    itoa!!!

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,28 +1,66 @@
 
-// #include "libft.h"
+#include "libft.h"
 
-// char *ft_itoa(int n)
+
+
+int nlen(int n)
+{
+	int i;
+	i = 0;
+	if (n < 0)
+	{
+		n = -n;
+		i++;
+	}
+	while (n >= 10)
+	{
+		n /= 10;
+		i++;
+	}
+	if (0 <= n <= 9)
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+
+char *ft_itoa(int n)
+{
+	char *result;
+	int sign;
+	int i;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	i = nlen(n);
+	result = (char *)malloc(i + 1);
+	if (!result)
+		return NULL;
+	result[i] = '\0';
+	sign = 1;
+	if (n < 0)
+	{
+		n = -n;
+		sign = 0;
+	}
+	while (i > 0)
+	{
+		result[i - 1] = n % 10 + '0';
+		n /= 10;
+		i--;
+	}
+	if (!sign)
+	{
+		result[0] = '-';
+	}
+	return result;
+}
+
+
+// int main()
 // {
-// 	char *str;
-// 	int i;
-// 	int sign;
-// 	char *result;
-// 	const char *p;
-// 	p = (const char *)n;
-// 	i = 0;
-// 	sign = 1;
-// 	result = NULL;
-// 	str = (char *)malloc(sizeof(char) * ((ft_strlen(p) + 1)));
-
-// 	if (!str || !n)
-// 		return NULL;
-// 	if (str[i] == '-')
-// 		sign = -1;
-// 	i++;
-// 	while (str[i])
-// 	{
-// 		result = str[i]*10 + result - '0';
-// 		i++;
-// 	}
-// 	return ((result));
+// 	int i = -10;
+// 	printf("%s\n",ft_itoa(i));
 // }
