c170f1c91de6f6072778d3a2d813b97986d22dcb
Yuxuan <nine.slide@gmail.com>
2023-05-18 22:58:49 +0200

    main file

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -8,7 +8,31 @@ char print_line(char *c)
 
 char	*get_next_line(int fd)
 {
-	ssize_t	n;
+	ssize_t n; 
+	size_t i;
+	char *buff;
 	n = 0;
+
+	buff = calloc(BUFFER_SIZE,1);
+
+	if(fd<=0)
+	{
+		return NULL;
+	}
+	while(1)
+	{
+		i = read(fd, buff+n, 1);
+		if(i<1)
+		{
+			buff[n]='\0';
+			return buff;
+		}
+		if(buff[n]=='\n')
+		{
+			buff[n+1]='\0';
+			return buff;
+		}
+		n += i;
+	}
 }
 
